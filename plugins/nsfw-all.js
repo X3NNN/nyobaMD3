0c96fe4f7f8eb1eeadf4aae66e20816db18d43ba
X3NNN <93273094+X3NNN@users.noreply.github.com>
2022-06-24 02:44:18 +0800

    anti lgbt

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -5,6 +5,7 @@ let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs']
 handler.tags = ['nsfw']
 handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs)$/i
+
 handler.premium = true
 //buatan hyzer, jgn hapus atuh 😊
 module.exports = handler
