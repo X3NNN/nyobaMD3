5c87140bbb73e34469a6ee486109d52d59dd3770
X3NNN <93273094+X3NNN@users.noreply.github.com>
2022-06-24 02:44:37 +0800

    anti lgbt

--- plugins/nsfw-all2.js
+++ plugins/nsfw-all2.js
@@ -5,5 +5,6 @@ let nyenye = `https://api.lolhuman.xyz/api/random/nsfw/${command}?apikey=${lolke
 handler.help = ['hollolewd', 'sideoppai', 'animefeets', 'animebooty', 'animethighss', 'animearmpits', 'lewdanimegirls', 'biganimetiddies']
 handler.tags = ['nsfw']
 handler.command = /^(hollolewd|sideoppai|animefeets|animebooty|animethighss|animearmpits|lewdanimegirls|biganimetiddies)$/i
+handler.premium = true
 //buatan hyzer, jgn hapus atuh 😊
 module.exports = handler
