171ed81f0a78e9e473a7189415e20f6b7a3479e3
X3NNN <93273094+X3NNN@users.noreply.github.com>
2022-06-25 01:17:54 +0800

    nerf merampok

--- plugins/rpg-merampok.js
+++ plugins/rpg-merampok.js
@@ -1,16 +1,16 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let pajak = 0.02
 let handler = async (m, { conn, text }) => {
-let dapat = (Math.floor(Math.random() * 10000000000000000))
+let dapat = (Math.floor(Math.random() * 100000))
   let who
   if (m.isGroup) who = m.mentionedJid[0]
   else who = m.chat
   if (!who) throw 'Tag salah satu lah'
   let __timers = (new Date - global.db.data.users[m.sender].lastrob)
-  let _timers = (3600000 - __timers) 
+  let _timers = (86400000 - __timers) 
   let timers = clockString(_timers)
   let users = global.db.data.users
-  if (new Date - global.db.data.users[m.sender].lastrob > 3600000){
+  if (new Date - global.db.data.users[m.sender].lastrob > 86400000){
   if (10000 > users[who].money) throw 'Target Gaada Uang bodoh, Kismin dia'
   users[who].money -= 80128896815 // dapat * 1
   users[m.sender].money += 80128896815 // dapat * 1
@@ -21,7 +21,7 @@ let dapat = (Math.floor(Math.random() * 10000000000000000))
 handler.help = ['merampok *@tag*']
 handler.tags = ['rpg']
 handler.command = /^merampok$/
-handler.limit = 20
+handler.limit = true
 handler.group = true
 
 module.exports = handler
@@ -30,7 +30,7 @@ function pickRandom(list) {
     return list[Math.floor(Math.random() * list.length)]
 }
 function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
+  let h = Math.floor(ms / 86400000)
   let m = Math.floor(ms / 60000) % 60
   let s = Math.floor(ms / 1000) % 60
   console.log({ms,h,m,s})
