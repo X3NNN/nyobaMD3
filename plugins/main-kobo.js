8ae3240a43e1c5a55f04da77fa16f02b233ed7c5
X3NNN <93273094+X3NNN@users.noreply.github.com>
2022-08-04 16:12:25 +0800

    Update main-kobo.js

--- plugins/main-kobo.js
+++ plugins/main-kobo.js
@@ -1,9 +1,18 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/3e07beef6f680de02b19d.png', m, { packname: "sticker by", author: "DXN" })
+conn.sendImageAsSticker(m.chat, pickRandom(stikerhuuu), m, { packname: "MLU S4", author: "MLUBOT" })
 }
 
-handler.customPrefix = /^(kobo)$/i
+handler.customPrefix = /^(huuu)$/i
 handler.command = new RegExp
 
 module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+let stikerhuuu = [
+ "https://telegra.ph/file/a03916d4eb0794f2680ff.png",
+ "https://telegra.ph/file/3e07beef6f680de02b19d.png",
+ "https://telegra.ph/file/09ed1ae78e24a0be1986c.png"
+]
